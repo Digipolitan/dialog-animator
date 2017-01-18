b82a4f675ead274c701abc9117b5313f85a0dc4f
Julien Sarazin <julien.sarazin@me.com>
2017-01-18 14:28:09 +0100

    minor refactoring regarding new coding coventions

---
+++ old/Toast/AnimatorBlurView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimatorBlurView.swift
+//  Toast
+//
+//  Created by Julien Sarazin on 28/12/2016.
+//  Copyright © 2016 Digipolitan. All rights reserved.
+//
+
+import UIKit
+
+class AnimatorBlurView: UIVisualEffectView {
+	var backDrop: Bool = true
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		if self.backDrop {
+			DGToastAnimator.default.dismiss()
+		}
+		super.touchesEnded(touches, with: event)
+	}
+}
