c2be3a7b8f1b641e90c5b826301cbacfe053c5f7
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-02-07 08:50:04 +0100

    update tests

--- Tests/DGDialogAnimatorTests/DGDialogAnimatorTests.swift
+++ Tests/DGDialogAnimatorTests/DGDialogAnimatorTests.swift
@@ -10,9 +10,4 @@ class DGDialogAnimatorTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
     }
-
-    func testDescription() {
-        let t = TemplateClass()
-        XCTAssertEqual(t.description, "TemplateDescription")
-    }
 }
